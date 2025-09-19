b7a79c0d6bb2e2bd3d09e7d3186f966283e51e28
Qing Dan（DANI） <danidan0406@gmail.com>
2025-09-18 22:42:27 -0700

    lab1

---
+++ F_to_C.js
@@ -0,0 +1,6 @@
+let f=55.4// Average temperature in Hohhot, Inner Mongolia for September 2023
+let c=(f-32)*5/9
+let k = (f + 459.67) * 5 / 9;
+console.log("Fahrenheit: " + f);
+console.log("Celsius: " + c);
+console.log("Kelvin: " + k);
